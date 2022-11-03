a8c729dca83f775b527d2367c16297fa5717bd04
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2022-11-03 20:46:40 +0200

    House Renting System

--- HouseRentingSystem/HouseRentingSystem.Core/Contracts/IHouseService.cs
+++ HouseRentingSystem/HouseRentingSystem.Core/Contracts/IHouseService.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace HouseRentingSystem.Core.Contracts
 {
     public interface IHouseService
-    {
+    { 
         Task<IEnumerable<HouseHomeModel>> LastThreeHouses();
     }
 }
